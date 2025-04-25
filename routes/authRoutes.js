da06f102c893198f1e9ad06f891618b352b91669
Harry <harryamilbox11@gmail.com>
2025-04-25 18:32:04 +0530

    Add test route to verify backend API

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { login, signup } = require('../controllers/authController');
 
+router.get('/test', (req, res) => {
+    res.json({ message: 'Auth API is working!' });
+});
+
 router.post('/login', login);
 router.post('/signup', signup);
 
